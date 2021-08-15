7501b620d8afd312a01652ff73a2d9fc8cda1ac9
Teo Chin Wei <1702348@sit.singaporetech.edu.sg>
2021-08-16 00:21:50 +0800

    update frontend

--- backend/src/controllers/controller.py
+++ backend/src/controllers/controller.py
@@ -2,8 +2,31 @@ from flask import jsonify, request
 from database.database import Database
 from database.firebase import db_create, db_update, db_read
 
+import requests
+import json
+
+
 def index():
     return "index"
+
+def safe_entry_api(nric):
+
+    url = "https://sandbox.api.safeentry-qr.gov.sg:443/partner/v1/entry"
+
+    payload = {
+        "subType": "uinfin",
+        "actionType": "checkout",
+        "sub": nric,
+        "venueId": "STG-180000001W-83338-SEQRSELFTESTSINGLE-SE",
+        "mobileno": "92376345"
+    }
+
+    r = requests.post(url, data=json.dumps(payload))
+
+    if r.status_code == 201:
+        return True
+    else:
+        return r.json()
 
 def from_db_get_vaccine_status(nric):
     data =  db_read("{}/{}".format("users",nric))
@@ -24,13 +47,18 @@ def checkin():
 
     # data from request body
     data = request.get_json()
-    print("data", data)
 
     nric = data.get("nric")
-    print("nric", nric)
+    nric = nric.upper()
 
     vaccine_status = from_db_get_vaccine_status(nric)
-    print("vaccine_status", vaccine_status)
+
+    safe_entry_response = safe_entry_api(nric)
+    print(safe_entry_response)
+
+    if safe_entry_response != True:
+        print("error safe entry")
+        return jsonify(safe_entry_response)
 
     if vaccine_status:
         # response 200
--- frontend/garfield/src/FormContainer.js
+++ frontend/garfield/src/FormContainer.js
@@ -9,7 +9,7 @@ class FormContainer extends Component {
     this.state = {
       nric: "",
       displayVacinneStatus: null,
-      vacinneStatusMessage: "",
+      vacinneStatusMessage: [],
     };
   }
 
@@ -28,9 +28,12 @@ class FormContainer extends Component {
 
     console.log(data);
 
+    let api_url = "https://todo-fddthxsyrq-as.a.run.app/api/checkin";
+    // let api_url = "http://localhost:8080/api/checkin";
+
     Axios({
       method: "post",
-      url: "http://localhost:8080/api/checkin",
+      url: api_url,
       headers: { "Content-Type": "application/json" },
       data: data,
     }).then((res) => {
@@ -38,19 +41,19 @@ class FormContainer extends Component {
       let vaccineStatus = res.data["vaccineStatus"];
       if (vaccineStatus === "not found") {
         this.setState({
-          vacinneStatusMessage: "user not found",
+          vacinneStatusMessage: ["Check in fail", "user not found"],
         });
       } else if (vaccineStatus === true) {
         this.setState({
-          vacinneStatusMessage: "you are vaccinated",
+          vacinneStatusMessage: ["Check in Success", "you are vaccinated"],
         });
       } else if (vaccineStatus === false) {
         this.setState({
-          vacinneStatusMessage: "you are not vaccinated",
+          vacinneStatusMessage: ["Check in Success", "you are not vaccinated"],
         });
       } else {
         this.setState({
-          vacinneStatusMessage: "error",
+          vacinneStatusMessage: ["Check in fail", res.data["message"]],
         });
       }
     });
@@ -59,25 +62,28 @@ class FormContainer extends Component {
   render() {
     return (
       <Container fluid>
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicNric">
-            <Form.Label>NRIC</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter nric"
-              name="nric"
-              onChange={this.handleChange}
-            />
-            <Form.Text className="text-muted">
-              We'll never share your nric with anyone else.
-            </Form.Text>
-          </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicNric">
+          <Form.Label>NRIC</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter nric"
+            name="nric"
+            onChange={this.handleChange}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your nric with anyone else.
+          </Form.Text>
+        </Form.Group>
 
-          <Button variant="primary" onClick={this.handleSubmit}>
-            Submit
-          </Button>
-        </Form>
-        {this.state.vacinneStatusMessage}
+        <Button variant="primary" onClick={this.handleSubmit}>
+          Submit
+        </Button>
+
+        {this.state.vacinneStatusMessage ? (
+          this.state.vacinneStatusMessage.map((item) => <p>{item}</p>)
+        ) : (
+          <p></p>
+        )}
       </Container>
     );
   }
