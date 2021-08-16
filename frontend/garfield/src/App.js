047214c6c442735f4e444fa3b0a625ac4c882678
Teo Chin Wei <1702348@sit.singaporetech.edu.sg>
2021-08-16 23:22:13 +0800

    update frontend ui changes

--- frontend/garfield/src/App.js
+++ frontend/garfield/src/App.js
@@ -1,12 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-import FormContainer from "./FormContainer";
+import GovYou from "./components/GovYou";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <FormContainer />
+        <GovYou />
       </header>
     </div>
   );
